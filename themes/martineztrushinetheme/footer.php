2e895c18ae53683cccb4c0ecf798c917910e3ef9
Yazan Deek <ydeek91@yahoo.com>
2022-02-27 10:53:26 -0700

    developed and styled site footer

--- themes/martineztrushinetheme/footer.php
+++ themes/martineztrushinetheme/footer.php
@@ -1,3 +1,22 @@
+<footer class="site-footer container">
+
+<div class="footer-content">
+<?php 
+$args = array(
+'theme_location' => 'main-menu',
+'container'=> 'nave',
+'container_class' => 'footer-menu'
+);
+wp_nav_menu( $args);
+?>
+
+<p class="copyright"> All Rights Reserved. <?php echo get_bloginfo('name')  . " " . date('Y'); ?>
+</div>
+
+
+</footer>
+
+
 
 </body>
 </html>
