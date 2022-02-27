f0ffeea1bff77b305b900313a1dc05f2ecfb9099
Yazan Deek <ydeek91@yahoo.com>
2022-02-27 08:26:13 -0700

    added bootstrap files to the folder

--- themes/martineztrushinetheme/functions.php
+++ themes/martineztrushinetheme/functions.php
@@ -11,3 +11,13 @@ function martineztrushine_menus(){
 
 // Hook
 add_action('init', 'martineztrushine_menus');
+
+// Adds Stylsheets and JS files 
+function martineztrushine_scripts(){
+
+// Normalize CSS
+wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+
+}
+
+add_action('wp_enqueue_scripts', 'martineztrushine_scripts');
--- themes/martineztrushinetheme/header.php
+++ themes/martineztrushinetheme/header.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
-    <!--Bootstrap link -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <?php wp_head(); ?>
+    
+    <!--Bootstrap core CSS -->
+    <link href="<?php bloginfo('template_url');?>/css/bootstrap.css" rel="stylesheet">
+    
+    <!--Custom styles for this template -->
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+    
 </head>
 <body>
     
