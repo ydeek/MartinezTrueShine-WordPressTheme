f967abd881b30a3998acab5cd6c144f7f2b187ce
Yazan Deek <ydeek91@yahoo.com>
2022-02-27 08:46:18 -0700

    added main stylesheet

--- themes/martineztrushinetheme/functions.php
+++ themes/martineztrushinetheme/functions.php
@@ -18,6 +18,14 @@ function martineztrushine_scripts(){
 // Normalize CSS
 wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
 
+// Google font 
+
+wp_enqueue_style('googlefont', 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700,900|Staatliches&display=swap', array(), '1.0.0' );
+
+// Main stylsheet 
+
+wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'googlefont'), '1.0.0' );
+
 }
 
 add_action('wp_enqueue_scripts', 'martineztrushine_scripts');
